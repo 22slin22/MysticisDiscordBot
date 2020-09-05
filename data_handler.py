b662be7dc29b6c2d914f2f500a18ef91fc9025ba
Nils Cremer <nils-cremer@t-online.de>
2020-09-05 23:33:27 +0200

    Add link_discord_to_brawl_id()

--- data_handler.py
+++ data_handler.py
@@ -1,14 +1,22 @@
 import json
 
 
+discord_to_brawl_id_file = "data/brawl_ids.json"
 player_stats_file_name = "data/players/{}_stats.json"
 player_ranked_file_name = "data/players/{}_ranked.json"
 
 
 def get_brawl_id_by_discord_id(discord_id):
-    with open("data/brawl_ids.json") as brawl_ids:
+    with open(discord_to_brawl_id_file) as brawl_ids:
         ids = json.load(brawl_ids)
         return ids[str(discord_id)]
+
+
+def link_discord_to_brawl_id(discord_id, brawl_id):
+    with open(discord_to_brawl_id_file, 'r+') as brawl_ids:
+        ids = json.load(brawl_ids)
+        ids[str(discord_id)] = brawl_id
+        json.dump(brawl_ids, ids)
 
 
 def get_player_stats_by_brawl_id(brawl_id):
