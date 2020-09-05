b5d94dc37ff731637a3651ab446e77acc347bace
Nils Cremer <nils-cremer@t-online.de>
2020-09-06 00:38:57 +0200

    Automatically update player stats

---
+++ brawlhalla.py
@@ -0,0 +1,48 @@
+import requests
+import configparser
+import threading
+import time
+from data_handler import get_all_brawl_ids, set_player_stats, set_player_ranked
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+api_key = config["Brawlhalla"]["api_key"]
+
+
+class BrawlhallaDataUpdater(threading.Thread):
+    def __init__(self):
+        super().__init__(daemon=True)
+        self.ids_to_update = []
+
+    def run(self):
+        while True:
+            to_update = 90
+            if len(self.ids_to_update) < 90:
+                new_ids = get_all_brawl_ids()
+                self.ids_to_update.extend(new_ids)
+                to_update = min(len(new_ids), 90)
+            for brawl_id in self.ids_to_update[:to_update]:
+                update_player_data(brawl_id)
+                time.sleep(0.1)
+            self.ids_to_update = self.ids_to_update[90:]
+
+            time.sleep(15*60)
+
+
+def get_player_stats(brawl_id):
+    response = requests.get(
+        "https://api.brawlhalla.com/player/{player}/stats?api_key={api_key}".format(player=brawl_id,
+                                                                                    api_key=api_key))
+    return response.json()
+
+
+def get_player_ranked(brawl_id):
+    response = requests.get(
+        "https://api.brawlhalla.com/player/{player}/ranked?api_key={api_key}".format(player=brawl_id,
+                                                                                     api_key=api_key))
+    return response.json()
+
+
+def update_player_data(brawl_id):
+    set_player_stats(brawl_id, get_player_stats(brawl_id))
+    set_player_ranked(brawl_id, get_player_ranked(brawl_id))
--- data_handler.py
+++ data_handler.py
@@ -13,15 +13,20 @@ def get_brawl_id_by_discord_id(discord_id):
 
 
 def link_discord_to_brawl_id(discord_id, brawl_id):
-    with open(discord_to_brawl_id_file, 'r+') as brawl_ids:
-        ids = json.load(brawl_ids)
-        ids[str(discord_id)] = brawl_id
+    try:
+        with open(discord_to_brawl_id_file, 'r') as brawl_ids:
+            ids = json.load(brawl_ids)
+    except json.decoder.JSONDecodeError:
+        ids = {}
+
+    ids[str(discord_id)] = brawl_id
+    with open(discord_to_brawl_id_file, 'w') as brawl_ids:
         json.dump(ids, brawl_ids)
 
 
 def get_player_stats_by_brawl_id(brawl_id):
-    with open(player_stats_file_name.format(brawl_id)) as player_info:
-        return json.load(player_info)
+    with open(player_stats_file_name.format(brawl_id)) as player_stats:
+        return json.load(player_stats)
 
 
 def get_player_stats_by_discord_id(discord_id):
@@ -29,11 +34,27 @@ def get_player_stats_by_discord_id(discord_id):
     return get_player_stats_by_brawl_id(brawl_id)
 
 
+def set_player_stats(brawl_id, stats):
+    with open(player_stats_file_name.format(brawl_id), 'w') as player_stats:
+        return json.dump(stats, player_stats)
+
+
 def get_player_ranked_by_brawl_id(brawl_id):
-    with open(player_ranked_file_name.format(brawl_id)) as player_info:
-        return json.load(player_info)
+    with open(player_ranked_file_name.format(brawl_id)) as player_ranked:
+        return json.load(player_ranked)
 
 
 def get_player_ranked_by_discord_id(discord_id):
     brawl_id = get_brawl_id_by_discord_id(discord_id)
     return get_player_ranked_by_brawl_id(brawl_id)
+
+
+def set_player_ranked(brawl_id, ranked):
+    with open(player_ranked_file_name.format(brawl_id), 'w') as player_ranked:
+        return json.dump(ranked, player_ranked)
+
+
+def get_all_brawl_ids():
+    with open(discord_to_brawl_id_file) as brawl_ids:
+        ids = json.load(brawl_ids)
+        return ids.values()
