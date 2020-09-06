2d8713e216f7b1019964d485d4642c490af85b09
warmachine13555 <benipapapapa123@gmail.com>
2020-09-06 02:22:13 +0200

    Add !rank detailed rank data

--- discord_bot.py
+++ discord_bot.py
@@ -1,5 +1,9 @@
-import discord
 import configparser
+
+import discord
+from discord import Embed
+from discord.utils import get
+
 from data_handler import get_player_ranked_by_discord_id, link_discord_to_brawl_id
 from brawlhalla import BrawlhallaDataUpdater
 
@@ -13,7 +17,6 @@ client = discord.Client()
 brawlhalla_data_updater = BrawlhallaDataUpdater()
 brawlhalla_data_updater.start()
 
-
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -25,7 +28,8 @@ async def on_message(message):
         return
 
     if message.content == '!verify':
-        await message.author.send('Hello ' + str(message.author) + ' ! \nTo get verified you need to post your Brawlhalla ID in here like that. \n```BrawlhallaID: "your ID"```')
+        await message.author.send('Hello ' + str(message.author) + ' ! \nTo get verified you need to post your Brawlhalla ID in here like that. \n'
+                                                                   '```BrawlhallaID: "your ID"```')
 
     if message.content.startswith('BrawlhallaID: '):
         brawl_id = message.content.split(" ")[1]
@@ -35,5 +39,33 @@ async def on_message(message):
 
     if message.content == '!rank':
         discord_id = message.author.id
-        await message.channel.send(get_player_ranked_by_discord_id(discord_id)['rating'])
+        get_tier = get_player_ranked_by_discord_id(discord_id)['tier']
+        tier = get_tier.split(" ")[0]
+        rank_role = get(message.author.guild.roles, name=tier)
+        name = get_player_ranked_by_discord_id(discord_id)['name']
+        region = get_player_ranked_by_discord_id(discord_id)['region']
+        rating = get_player_ranked_by_discord_id(discord_id)['rating']
+        peak_rating = get_player_ranked_by_discord_id(discord_id)['peak_rating']
+        wins = get_player_ranked_by_discord_id(discord_id)['wins']
+        games = get_player_ranked_by_discord_id(discord_id)['games']
+        losses = games - wins
+        win_perecentage = wins / (wins + losses) * 100
+        global_rank = get_player_ranked_by_discord_id(discord_id)['global_rank']
+        rounded_win_percentage = round(win_perecentage)
+        await message.author.add_roles(rank_role)
+        await message.channel.send('```Player Data'
+                                   '\n\nName\t\t\t\t\t\t\t\tRegion\n'
+                                   + str(name) + '\t\t\t\t' + str(region) + '\n\n'
+                                   '\t\t\t\t\tRanked Data\n\n'
+                                   '\t\t\t\t\t1v1 Ranking\n\t\t\t\t\t'
+                                   + str(get_tier) + '\n\t\t\t\t\t'
+                                   '(' + str(rating) + ' / ' + str(peak_rating) + ' Peak)\n\t\t\t\t\t'
+                                   + str(wins) + ' Wins / ' + str(losses) + ' Losses\n\t\t\t\t\t'
+                                   '(Total Games ' + str(games) + ')\n\t\t\t\t\t'
+                                   + str(rounded_win_percentage) + '% Winrate\n\t\t\t\t\t'
+                                   'Global Rank ' + str(global_rank) + '```')
+
+    if message.content == "!test":
+        discord_id = message.author.id
+        await message.channel.send(get_player_ranked_by_discord_id(discord_id)['tier'])
 client.run(token)
